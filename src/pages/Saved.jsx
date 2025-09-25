ce40549275620756ab407195f7966bb73ea890a5
czhengcs <zhengcathy2@gmail.com>
2025-09-24 23:49:44 -0700

    Create Saved Jobs page

---
+++ src/pages/Saved.jsx
@@ -0,0 +1,8 @@
+function Saved() {
+    return <div className="saved-empty">
+        <h2>No Saved Jobs Yet</h2>
+        <p>Start saving jobs and they will appear here</p>
+    </div>
+}
+
+export default Saved
