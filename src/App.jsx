3f26783c246040c8c0d3c8a642da8357255008a2
czhengcs <zhengcathy2@gmail.com>
2025-09-25 00:13:54 -0700

    Implement page navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import JobCard from "./components/JobCard";
+import Saved from "./pages/Saved";
 import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom"
+import NavBar from "./components/NavBar";
 
 function App() {
 
   return (
-    <>
-      <Home />
-    </>
+    <div>
+      <NavBar />
+      <main className='main-content'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/saved' element={<Saved />} />
+        </Routes>
+      </main>
+    </div>
   );
 }
 
