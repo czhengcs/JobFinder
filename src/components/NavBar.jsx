09d0b03ee1cb523207f012f7db7c9fcfb1001051
czhengcs <zhengcathy2@gmail.com>
2025-09-30 20:53:56 -0700

    Add styling for NavBar component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,11 @@
 import { Link } from "react-router-dom";
+import logo from "../assets/maple.png";
+import "../css/NavBar.css"
 
 function NavBar() {
     return <nav className="navbar">
         <div className="navbar-brand">
+            <img src={logo} alt="logo.png" className="navbar-logo" />
             <Link to="/">JobFinder</Link>
         </div>
         <div className="navbar-links">
